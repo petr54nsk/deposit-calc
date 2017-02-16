b2f009d7039f3c56808615e3948b6ccd5c324a7e
StMark <prizrack91@gmail.com>
2017-02-17 02:22:00 +0700

    Дописал 3 и 4 функции

--- nain.c
+++ nain.c
@@ -17,12 +17,20 @@ int one(int d, int ye){
 }
 
 int two(int d, int ye){
-
+	int p,s=ye;
+	if (d=1)p=0,06;
+	if (d=2)p=0,08;
+	s=s+s*p;
+	return s;
 
 }
 
 int three(int d, int ye){
-
+	int p,s=ye;
+	if (d=1)p=0,12;
+	if (d=2)p=0,15;
+	s=s+s*p;
+	return s;
 
 }
 
