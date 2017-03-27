4fad29c1ef929f43036f6118334636aee0f3002e
Марченко Александр Александрович <ip615s16@mwp09.csc.local>
2017-03-27 16:04:22 +0700

    end

--- src/deposit.cpp
+++ src/deposit.cpp
@@ -9,8 +9,8 @@ int zero(int &d, int ye){
 int one(int &d, int ye){
 	int s=ye;
 	double p;
-	if (d=1){p=0,02;};
-	if (d=2){p=0,03;};
+	if (d==1){p=0.02;};
+	if (d==2){p=0.03;};
 	s=s+s*p;
 	return s;
 }
@@ -19,18 +19,18 @@ int two(int &d, int ye){
 	int s=ye;
 	cout << d<< endl;
 	double p;
-	if (d=1){p=0,6;};
-	if (d=2){p=8*pow(10,-2);};
+	if (d==1){p=0.6;};
+	if (d==2){p=0.8;};
 	s=s+s*p;
 	return s;
 
 }
 
-int three(int d, int ye){
-	int s=ye,q=d;
+int three(int &d, int ye){
+	int s=ye;
 	double p;
-	if (d=1){p=0,12;};
-	if (d=2){p=0,15;};
+	if (d==1){p=0.12;};
+	if (d==2){p=0.15;};
 	s=s+s*p;
 	return s;
 
